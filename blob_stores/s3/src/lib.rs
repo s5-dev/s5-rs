584b27a2dea120ef09ac652a6f0c2fe536f6edc9
redsolver <red@s5.pro>
2025-09-05 15:50:52 +0200

    feat(store/s3): implement config, exists method, delete method and provide method

--- blob_stores/s3/src/lib.rs
+++ blob_stores/s3/src/lib.rs
@@ -1,13 +1,22 @@
+use anyhow::anyhow;
 use bytes::Bytes;
 use futures::Stream;
-use s3::{Bucket, Region, creds::Credentials};
+use s3::{Bucket, Region, creds::Credentials, error::S3Error};
 use s5_core::{
     blob::location::BlobLocation,
     store::{StoreFeatures, StoreResult},
 };
+use std::u64;
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct S3StoreConfig {}
+pub struct S3StoreConfig {
+    endpoint: String,
+    #[serde(default)]
+    region: String,
+    bucket_name: String,
+    access_key: String,
+    secret_key: String,
+}
 
 #[derive(Debug, Clone)]
 pub struct S3Store {
@@ -17,14 +26,14 @@ pub struct S3Store {
 impl S3Store {
     pub fn create(config: S3StoreConfig) -> Self {
         let bucket = Bucket::new(
-            "test-rust-s3",
+            &config.bucket_name,
             Region::Custom {
-                endpoint: "http://127.0.0.1:8080".to_string(),
-                region: "".to_string(),
+                endpoint: config.endpoint,
+                region: config.region,
             },
             Credentials::new(
-                Some("ACCESS_KEY_ID"),
-                Some("SECRET_ACCESS_KEY"),
+                Some(&config.access_key),
+                Some(&config.secret_key),
                 None,
                 None,
                 None,
@@ -44,19 +53,28 @@ impl s5_core::store::Store for S3Store {
         path: &str,
         stream: Box<dyn Stream<Item = Bytes> + Send + Unpin + 'static>,
     ) -> StoreResult<()> {
-        todo!()
-    }
-
-    fn features(&self) -> StoreFeatures {
-        todo!()
-    }
-
-    async fn exists(&self, path: &str) -> StoreResult<bool> {
-        todo!()
+        todo!("implement")
     }
 
     async fn put_bytes(&self, path: &str, bytes: Bytes) -> StoreResult<()> {
-        todo!()
+        todo!("implement")
+    }
+
+    fn features(&self) -> StoreFeatures {
+        StoreFeatures {
+            supports_rename: false,
+            case_sensitive: true,
+            recommended_max_dir_size: u64::MAX,
+        }
+    }
+
+    async fn exists(&self, path: &str) -> StoreResult<bool> {
+        match self.bucket.head_object(path).await {
+            Ok((_, 200)) => Ok(true),
+            Ok((_, 404)) => Ok(false),
+            Ok((_, code)) => Err(anyhow!("unexpected http status code {code}")),
+            Err(e) => Err(e.into()),
+        }
     }
 
     async fn open_read_stream(
@@ -78,14 +96,16 @@ impl s5_core::store::Store for S3Store {
     }
 
     async fn delete(&self, path: &str) -> StoreResult<()> {
-        todo!()
+        self.bucket.delete_object(path).await?;
+        Ok(())
     }
 
     async fn rename(&self, old_path: &str, new_path: &str) -> StoreResult<()> {
-        todo!()
+        panic!("not supported by this store")
     }
 
     async fn provide(&self, path: &str) -> StoreResult<Vec<BlobLocation>> {
-        todo!()
+        let res = self.bucket.presign_get(path, 86400, None).await?;
+        Ok(vec![BlobLocation::Url(res)])
     }
 }
